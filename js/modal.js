214c199a1af10cce3c3e52c5ec55c3377946117a
k2Forest-Man <LesnoiChelovek@yandex.ru>
2022-03-20 04:53:15 +0300

    модальное окно открывается при нажатии по всем кнопка - узнать больше

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,17 @@
-const modalButton = document.querySelector('.more')
+const modalButton = document.querySelectorAll('.more')
 const modal = document.querySelector('.modal')
 const overlay = modal.querySelector('.overlay')
 const closeButton = modal.querySelector('.modal__close')
 
-
-
-modalButton.addEventListener('click', () => {
-  modal.classList.remove('hidden')
+modalButton.forEach((buttons) => {
+  buttons.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+  })
 })
+
+// modalButton.addEventListener('click', () => {
+//   modal.classList.remove('hidden')
+// })
 
 overlay.addEventListener('click', () => {
   modal.classList.add('hidden')
